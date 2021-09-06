f4d5f378e258158129711cce720eb337bb6fb139
dtinth on MBP M1 <dtinth@spacet.me>
2021-09-06 23:27:52 +0700

    Generate example images

---
+++ example/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+rm -rf images
+mkdir -p images
+
+generate() {
+  pnpx playwright screenshot --viewport-size="1280,720" "file://$PWD/$1" images/$2.png
+}
+
+generate "build/site/vue3-apidocs-example/api/vue_reactivity.html" reactivity
+generate "build/site/vue3-apidocs-example/api/vue_reactivity.html#vue_reactivity_reactive_function_1" computed
+generate "build/site/vue3-apidocs-example/api/vue_shared_PatchFlags_enum.html" patchflags
